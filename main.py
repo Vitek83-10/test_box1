91b328e6ae3e85c0d085dd31127c1413d4f4523a
Vitek83-10 <vnedovesov83@gmail.com>
2025-06-15 23:50:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
+import os
 from pyrogram import Client
-import asyncio
 
-api_id = 12345678  # ← сюда вставь свой api_id
-api_hash = "your_api_hash_here"  # ← сюда вставь свой api_hash
-bot_token = "8139101323:AAEcT5Tny_PEerVqFuLOtjqdBekpivq4G-k"  # ← это уже твой токен
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
 
-app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+app = Client("my_bot", api_id=api_id, api_hash=api_hash)
 
 @app.on_message()
-async def handler(client, message):
-    await message.reply("Бот работает ✅")
+def handle(client, message):
+    print(f"Новое сообщение: {message.text}")
 
-async def main():
-    await app.start()
-    print("Бот запущен...")
-    await asyncio.Event().wait()  # Ожидаем бесконечно, чтобы не завершалось
-
-if __name__ == "__main__":
-    asyncio.run(main())
+app.run()
